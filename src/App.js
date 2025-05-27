466c8599ce8ecad20f501f87aaaa73521944d767
Vishruti24 <vishu2412004@gmail.com>
2025-05-27 21:33:02 +0530

    COMMIT2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForms from './components/TextForms';
 
 
 
 function App() {
+  const[Mode,setMode]=useState("light");//wheather darkmode is enable or not
+
+  const toggleMode = () => {
+    if(Mode ==="light"){
+    setMode("dark");
+    document.body.style.backgroundColor="#062a5e";
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor="white";
+    }
+  }
   return (
     <>
 {/* pass name as var is props and pass it in navbar props and in classname give its title*/}
-<Navbar title="Textutills" aboutText="About us"/>
+ <Navbar title="Textutills"mode={Mode} toggleMode={toggleMode}/> 
+  {/* <Navbar title="Textutills" aboutText="About us" mode={Mode} toggleMode={toggleMode}/> */}
 {/* <Navbar/> */}
 
 <div className="container my-3">
-<TextForms heading="Enter the text to analyze below"/>
+ <TextForms heading="Enter the text to analyze below" mode={Mode}/> 
+ {/* <About/>  */}
 </div>
-
     </>
   );
 }
-
 export default App;
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import Navbar from './components/Navbar';
+// import TextForms from './components/TextForms';
+// function App() {
+//   return (
+//     <>
+//       {/* pass name as var is props and pass it in navbar props and in classname give its title*/}
+//       {/* <Navbar title="Textutills" aboutText="About us" /> */}
+//       <Navbar/>
+
+//       <div className="container my-3">
+//         <TextForms heading="Enter the text to analyze below" />
+//       </div>
+
+//     </>
+//   );
+// }
+
+// export default App;
--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -6,34 +6,78 @@ export default function TextForms(props) {
 
     const handleUpClick = () => {
         //console.log("Uppercase was clicked" + text);
-        let newText=text.toLocaleUpperCase();
+        let newText=text.toUpperCase();
         setText("You have clicked on handleUpClick");//because of state change it is re rander..
         setText(newText)
     }
-    
+    const handleLoClick = () => {
+      //console.log("Lowercase was clicked" + text);
+      let newText=text.toLowerCase();
+      setText("You have clicked on handleLoClick");//because of state change it is re rander..
+      setText(newText)
+   }
+   const handleClearClick = () => {
+    //console.log("Lowercase was clicked" + text);
+    let newText='';
+    setText("You have clicked on handleLoClick");//because of state change it is re rander..
+    setText(newText)
+ }
+ const handleNubClick = () => {
+  //console.log("Lowercase was clicked" + text);
+  let newText=(text.match(/\d+/g) || []).join(" ");
+  setText("You have clicked on handleNubClick");//because of state change it is re rander..
+  setText(newText)
+}
+const handleLinkClick = () => {
+  //console.log("Lowercase was clicked" + text);
+  let newText= (text.match(/(https?:\/ \/[^\s]+|www\.[^\s]+)/g) || []).join(" ");
+  setText("You have clicked on handleLinkClick");//because of state change it is re rander..
+  setText(newText)
+}
+const handleExtraspace = () =>{
+  let newText=text.split(/[ ]+/).join(" ");
+setText("you have clicked on handleExtraspace");
+setText(newText);
+
+}
+
+
     const handleOnChange = (event)  =>{
         //console.log("On change");
         setText(event.target.value);//we can type in text new value 
     }
 
-
-
     //hook always put under the function we use array destructing we set text and setText both
     //text="new text"; WRONG WAYwe can't declare var or state like this
     //setText("new Text"); CORRECT WAY
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
    
- 
-    
+
+
   return (
-    <div>
+    <>
+    <div className='container' style={{color:props.mode!=='dark'?'black':'white'}}>
         <h1>{props.heading}</h1>
 <div className="mb-3">
   {/* <label for="myBox" class="form-label">Example textarea</label> */}
-  <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
+  <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.Mode==='dark'?'gray':'white', color:props.mode!=='dark'?'black':'white'}}id="myBox" rows="10"></textarea>
 </div>
-<button className="btn btn primary" onClick={handleUpClick}>Convert to UpperCase</button>
+<button className="btn btn-secondary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+<button className="btn btn-secondary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
+<button className="btn btn-secondary mx-1" onClick={handleClearClick}>Clear Text</button>
+<button className="btn btn-secondary mx-1" onClick={handleNubClick}>Extract Number</button>
+<button className="btn btn-secondary mx-1" onClick={handleLinkClick}>Extract Link</button>
+<button className="btn btn-secondary mx-1" onClick={handleExtraspace}>Remove Extra space</button>
+
     </div>
+    <div className="container" my-3  style={{color:props.mode!=='dark'? 'black':'white'}}>
+      <h2>Your text summary</h2>
+      <p>{text.split(" ").length} word and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes to read</p>
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter in the text for preview it"}</p>
+    </div>
+    </>
   )
 }
 //how to handle event
